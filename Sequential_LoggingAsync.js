f3fb8cdc3ffa527500e53746be99206a25cee152
Mohd Kaif <mohdkaifmohdkaif22@gmail.com>
2025-11-17 22:44:11 -0800

    Sequential_LoggingAsync

---
+++ Sequential_LoggingAsync.js
@@ -0,0 +1,13 @@
+function wait(ms)
+{
+        return new Promise(resolve=>setTimeout(resolve,ms));
+}
+async function run()
+{
+        for(let i=1; i<=3; i++)
+        {
+                await wait(500);
+                console.log(i);
+        }
+}
+run();
